38a45933600c17e6869f016a882ca4802926eb40
crstiangutierrez <gutiguti024@gmail.com>
2021-11-08 18:54:12 -0500

    Se creó un Splash Screen

---
+++ app/src/main/java/com/magit/mpi01/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.magit.mpi01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
--- app/src/main/java/com/magit/mpi01/MainActivity.java
+++ app/src/main/java/com/magit/mpi01/MainActivity.java
@@ -2,13 +2,41 @@ package com.magit.mpi01;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        Animation animacion1 = AnimationUtils.loadAnimation(this, R.anim.desplazamiento_arriba);
+        Animation animacion2 = AnimationUtils.loadAnimation(this, R.anim.despazamiento_abajo);
+
+        TextView holaTextView = findViewById(R.id.holaTextView);
+        ImageView logoImageView = findViewById(R.id.logoImageView);
+
+        holaTextView.setAnimation(animacion2);
+        logoImageView.setAnimation(animacion1);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+
+
     }
 }
