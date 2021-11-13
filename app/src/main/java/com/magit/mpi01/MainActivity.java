fbb7a60075af79bff7ec176e8f8603e18b7989c3
crstiangutierrez <gutiguti024@gmail.com>
2021-11-13 09:46:56 -0500

    Log In mejorado

--- app/src/main/java/com/magit/mpi01/MainActivity.java
+++ app/src/main/java/com/magit/mpi01/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
         Animation animacion1 = AnimationUtils.loadAnimation(this, R.anim.desplazamiento_arriba);
